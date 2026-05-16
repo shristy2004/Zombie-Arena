6d3240f441720bb5249a584fe03271df20085133
Shristy <shristy712@gmail.com>
2026-05-16 09:11:14 +0530

    Add Bullet class with position and shape attributes

--- Bullet.cpp
+++ Bullet.cpp
@@ -1,3 +1,18 @@
  #include <SFML/Graphics.hpp>
 #include<cmath>
 using namespace sf;
+class Bullet
+{
+private:
+	Vector2f m_Position;
+	RectangleShape m_BulletShape;
+	bool m_InFlight = false;
+	float m_BulletSpeed = 1000;
+	float m_BulletDistanceX;
+	float m_BulletDistanceY;
+	float m_XTarget;
+	float m_YTarget;
+	float m_MaxX;
+	float m_MinX;
+	float m_MaxY;
+	float m_MinY;
