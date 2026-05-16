52f129279f35a728ad5d95eb517a55965ccd73ce
Shristy <shristy712@gmail.com>
2026-05-16 09:14:37 +0530

    Update Bullet::update to handle movement and range
    Implement bullet movement and out-of-range check.

--- Bullet.cpp
+++ Bullet.cpp
@@ -87,3 +87,19 @@ RectangleShape Bullet::getShape()
 
 void Bullet::update(float elapsedTime)
 {
+
+// Update the bullet position variables
+	m_Position.x += m_BulletDistanceX * elapsedTime;
+	m_Position.y += m_BulletDistanceY * elapsedTime;
+
+	// Move the bullet
+	m_BulletShape.setPosition(m_Position);
+
+	// Has the bullet gone out of range?
+	if (m_Position.x < m_MinX || m_Position.x > m_MaxX ||
+		m_Position.y < m_MinY || m_Position.y > m_MaxY)
+	{
+		m_InFlight = false;
+	}
+
+}
